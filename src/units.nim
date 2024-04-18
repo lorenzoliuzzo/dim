fd647d03fb6a774e206a96d69db917d82e811fc1
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-04-18 16:02:28 +0200

    feat(unitSystem): added SystemInfo type definition and its quantities definitions.

--- src/units.nim
+++ src/units.nim
@@ -1,9 +1,16 @@
 import 
-    units/[utils, unitInfo]
+    units/[utils, unitInfo, prefix]
 
 from sequtils import mapIt
+
 
 macro unitSystem*(name, impl: untyped) =
     if name.kind != nnkIdent: error("expected system name", name)
 
     let info = newSystemInfo(name, impl.mapIt(it.getUnitInfo))
+
+    result = newStmtList()
+    result.add info.typeDefinition
+
+    for i, _ in info.units:
+        result.add info.quantityDefinition(i)

d0dadd7ac4e6642caf5277bd856d5e4bca8b4441
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-04-18 14:59:14 +0200

    test(unitSystem): testing the unitSystem macro to generate a new unit system by listing quantities, units and symbols.

---
+++ tests/test_unitSystem.nim
@@ -0,0 +1,20 @@
+import macros
+import units
+
+unitSystem si: 
+    Length:          meters(m)
+    Mass:            kilograms(kg)
+    Time:            seconds(s)
+    ElectricCurrent: amperes(A)
+    Temperature:     kelvins(K)
+    Amount:          moles(mol)
+
+
+dumpTree:
+    unitSystem si: 
+        Length:          meters(m)
+        Mass:            kilograms(kg)
+        Time:            seconds(s)
+        ElectricCurrent: amperes(A)
+        Temperature:     kelvins(K)
+        Amount:          moles(mol)

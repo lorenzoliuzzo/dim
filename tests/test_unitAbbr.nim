418bcf03160c4e4abcc6490f15ceac28261e13ca
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-04-18 19:56:30 +0200

    test: testing unitPrefix and unitAbbr with SI units.

---
+++ tests/test_unitAbbr.nim
@@ -0,0 +1,15 @@
+from units import unitAbbr
+import test_unitPrefix, test_unitSystem
+
+unitAbbr:
+    ps  = pico.seconds
+    ns  = nano.seconds
+    mis = micro.seconds
+    ms  = mili.seconds
+
+    nm  = nano.meters
+    um  = micro.meters
+    mm  = mili.meters
+    dm  = deci.meters
+    cm  = centi.meters
+    km  = kilo.meters

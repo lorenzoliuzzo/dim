45d986e50028a7b924dfd44dc2a0dc569ab027de
lorenzoliuzzo <lorenzo.liuzzo1@studenti.unimi.it>
2024-04-18 16:03:47 +0200

    test(unitSystem): tested quantity definition and procedure.

--- tests/test_unitSystem.nim
+++ tests/test_unitSystem.nim
@@ -9,12 +9,6 @@ unitSystem si:
     Temperature:     kelvins(K)
     Amount:          moles(mol)
 
-
-dumpTree:
-    unitSystem si: 
-        Length:          meters(m)
-        Mass:            kilograms(kg)
-        Time:            seconds(s)
-        ElectricCurrent: amperes(A)
-        Temperature:     kelvins(K)
-        Amount:          moles(mol)
+let a = Length(10)
+let b = 10.meters
+assert b is Length
